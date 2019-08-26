b7e57bc6804b9df3b9f759872da79d6142a56e2c
Grzegorz Sienkiewicz <grzegorz.sienkiewicz@eu.jll.com>
2019-08-26 21:19:11 +0200

    Add variables

---
+++ js/script.js
@@ -0,0 +1,25 @@
+function printMessage(msg) {
+  var div = document.createElement("div");
+  div.innerHTML = msg;
+  document.getElementById("messages").appendChild(div);
+}
+
+function clearMessages() {
+  document.getElementById("messages").innerHTML = "";
+}
+
+var computerMove;
+computerMove = "kamień";
+printMessage(
+  "Zagrałem " + computerMove + "! Jeśli Twój ruch to papier, to wygrywasz!"
+);
+
+var playerMove;
+playerMove = "papier";
+printMessage(
+  "Zagrałem " +
+    computerMove +
+    "! Jeśli Twój ruch to" +
+    playerMove +
+    " , to wygrywasz!"
+);
